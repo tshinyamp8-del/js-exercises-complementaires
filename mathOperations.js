33f71f243bd2ea0e8c96b6b6e6d1978729719953
Parfait Kamey Tshinyam <tshinyamp8@gmail.com>
2025-12-01 17:16:48 +0200

    Operations mathematiques simples

---
+++ mathOperations.js
@@ -0,0 +1,16 @@
+// 1. Demander deux nombres à l'utilisateur
+let nombre1 = Number(prompt("Entrez le premier nombre :"));
+let nombre2 = Number(prompt("Entrez le deuxième nombre :"));
+
+// 2. Calculs
+let somme = nombre1 + nombre2;
+let difference = nombre1 - nombre2;
+let produit = nombre1 * nombre2;
+let division = nombre1 / nombre2;
+
+// 3. Affichage dans la console
+console.log("Somme: " + somme);
+console.log("Différence: " + difference);
+console.log("Produit: " + produit);
+console.log("Division: " + division);
+
